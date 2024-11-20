3933bfbe73e2ff5ab95e53a98e5cfeee74df2496
暮蝶iso <68836662+CloveIso@users.noreply.github.com>
2024-11-20 09:51:52 +0800

    Update hook_copyright.py

--- scripts/hook_copyright.py
+++ scripts/hook_copyright.py
@@ -3,4 +3,4 @@
 from datetime import datetime
 
 def on_config(config, **kwargs):
-    config.copyright = f"版权所有 © 2022-{datetime.now().year} Ecool的知识花园"
+    config.copyright = f"版权所有 © 2022-{datetime.now().year} 暮蝶iso"
